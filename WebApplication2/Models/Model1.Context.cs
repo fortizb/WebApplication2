b30704e36fa6cbd8ad26f863070ed525c6e194d3
JUAN CARLOS ROBERTO REBOLLEDO MESTRE <juan.rebolledo15@inacapmail.cl>
2018-12-04 11:59:24 -0300

    Correccion contexto

--- WebApplication2/Models/Model1.Context.cs
+++ WebApplication2/Models/Model1.Context.cs
@@ -31,7 +31,6 @@ namespace WebApplication2.Models
         public virtual DbSet<guias> guias { get; set; }
         public virtual DbSet<hojaRuta> hojaRuta { get; set; }
         public virtual DbSet<hojaRutaDetalle> hojaRutaDetalle { get; set; }
-        public virtual DbSet<rol> rol { get; set; }
         public virtual DbSet<usuario> usuario { get; set; }
         public virtual DbSet<vehiculo> vehiculo { get; set; }
     }
--- WebApplication2/Models/Model1.Designer.cs
+++ WebApplication2/Models/Model1.Designer.cs
@@ -1,3 +1,4 @@
+﻿// La generación de código T4 está habilitada para el modelo 'C:\Users\Muss\Source\Repos\fortizb\WebApplication2\WebApplication2\Models\Model1.edmx'. 
 // Para habilitar la generación de código heredada, cambie el valor de la propiedad del diseñador 'Estrategia de generación de código'
 // por 'ObjectContext heredado'. Esta propiedad está disponible en la ventana Propiedades cuando se abre
 // el modelo en el diseñador.
