28b66d87382eb802f4799e99ca652a5fa50e0756
Felipe Ortiz <flop19@gmail.com>
2018-12-04 11:07:09 -0300

    (Sin versión) Cambios después del merge

--- WebApplication2/Models/Model1.Context.cs
+++ WebApplication2/Models/Model1.Context.cs
@@ -13,10 +13,10 @@ namespace WebApplication2.Models
     using System.Data.Entity;
     using System.Data.Entity.Infrastructure;
     
-    public partial class dimacodevEntities : DbContext
+    public partial class dimacodevEntities1 : DbContext
     {
-        public dimacodevEntities()
-            : base("name=dimacodevEntities")
+        public dimacodevEntities1()
+            : base("name=dimacodevEntities1")
         {
         }
     
@@ -31,7 +31,6 @@ namespace WebApplication2.Models
         public virtual DbSet<guias> guias { get; set; }
         public virtual DbSet<hojaRuta> hojaRuta { get; set; }
         public virtual DbSet<hojaRutaDetalle> hojaRutaDetalle { get; set; }
-        public virtual DbSet<rol> rol { get; set; }
         public virtual DbSet<usuario> usuario { get; set; }
         public virtual DbSet<vehiculo> vehiculo { get; set; }
     }
--- WebApplication2/Models/usuario.cs
+++ WebApplication2/Models/usuario.cs
@@ -32,6 +32,5 @@ namespace WebApplication2.Models
     
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<hojaRuta> hojaRuta { get; set; }
-        public virtual rol rol { get; set; }
     }
 }
